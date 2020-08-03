1184faa4f3a6ab84cf8542cafd942c12f6cb0e04
kobi <kobishaked1@gmail.com>
2020-08-03 23:08:03 +0300

    made the connection between ChatBox component and firebase. storing text content in DB and update notifications.

--- src/pages/chatBox/ChatBox.js
+++ src/pages/chatBox/ChatBox.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Card } from 'react-bootstrap'
 import ReactLoading from 'react-loading'
 import 'react-toastify/dist/ReactToastify.css'
 import firebase from '../../services/firebase'
 import Images from '../../projectImages/projectImages'
-import moment from 'react-moment'
+import moment from 'moment'
 import './ChatBox.css'
 import LoginStrings from '../Signin/LoginStrings'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { firestore } from 'firebase'
 
 
 export default function ChatBox(props) {
@@ -18,11 +19,62 @@ export default function ChatBox(props) {
     const [currnetUserDocumentId, setCurrnetUserDocumentId] = useState(localStorage.getItem(LoginStrings.FirebaseDocumentId))
     const [currentPeerUser, setCurrentPeerUser] = useState(props.currentPeerUser)
     const [inputValue, setInputValue] = useState(null)
+    // const [groupChatId, setGroupChatId] = useState(null)
+    const [currentPeerUserMessages, setCurrentPeerUserMessages] = useState(null)
+    // const [removeListener, setRemoveListener] = useState(null)
+    const [currentPhotoFile, setCurrentPhotoFile] = useState(null)
+    // const [listMessage, setListMessage] = useState([])
+
+    const listMessage = useRef([])
+    const groupChatId = useRef()
+    const removeListener = useRef()
+    const refInput = useRef(null)
+    
+
+    // useEffect(() => {
+    //     scrollToBottom();
+    // }
+    // );
 
     useEffect(() => {
-        
-       
-    }, [currentPeerUser])
+        setCurrentPeerUser(props.currentPeerUser)
+        getListHistory()
+
+        //these purpose lines is to get the message object of the "peer user" that we
+        //want to chat with (who we click on) 
+        firebase.firestore().collection('users').doc(currentPeerUser.documentKey).get()
+            .then((docRef) => {
+                setCurrentPeerUserMessages(docRef.data().messages)
+            })
+
+    }, [props.currentPeerUser]);
+
+
+    function getListHistory() {
+        setIsLoading(true)
+        if (hashString(currnetUserId) <= hashString(currentPeerUser.id)) {
+            groupChatId.current = `${groupChatId.current} - ${currentPeerUser.id}`
+        }
+        else {
+            groupChatId.current = `${currentPeerUser.id} - ${groupChatId.current}`
+        }
+        //get history and listen to new data added
+        removeListener.current = firebase.firestore().collection('Messages').doc(groupChatId.current)
+        .collection(groupChatId.current).onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if (change.type === LoginStrings.DOC){
+                    listMessage.current.push(change.doc.data())
+                }
+            })
+            setIsLoading(false)
+        },
+        err => {
+            console.log(err)
+        }
+        )
+
+    }
+
 
 
     function messageContentChange(e) {
@@ -33,18 +85,74 @@ export default function ChatBox(props) {
 
     }
 
-    function messageContentChange() {
+    function messageContentChange(e) {
+        setInputValue(e.target.value)
 
     }
 
-    function onKeyPress() {
-
+    function onKeyPress(e) {
+        if (e.key === 'Enter') {
+            onSendMessage(inputValue, 0)
+        }
     }
 
-    function onSendMessage(a, b) {
+    // function scrollToBottom() {
+    //     if (messagesEnd) {
+    //         messagesEnd.scrollIntoView({})
+    //     }
+    // }
 
+    function onSendMessage(content, type) {
+        let notificationMessages = []
+        //checking if the content is empty, if so, dont do nothing
+        if (content.trim() === '') {
+            return
+        }
+        const timestamp = moment().valueOf().toString()
+        const itemMessage = {
+            idFrom: currnetUserId,
+            idTo: currentPeerUser,
+            timestamp: timestamp,
+            content: content.trim(),
+            type: type
+        }
+        //store the message object (itemMessage) in a new collection called Messages
+        firebase.firestore().collection('Messages').doc(groupChatId.current)
+            .collection(groupChatId.current).doc(timestamp).set(itemMessage).then(() => {
+                setInputValue('')
+            })
+        //update the peer user messages object (for notification purpose)
+        //inside the users collection
+        currentPeerUserMessages.map(item => {
+            if (item.notificationId = !currnetUserId) {
+                notificationMessages.push(
+                    {
+                        notificationId: item.notificationId,
+                        number: item.number
+                    }
+                )
+            }
+        })
+        // console.log(currentPeerUser.documentKey)
+        firebase.firestore().collection('users').doc(currentPeerUser.documentKey)
+            .update({
+                messages: notificationMessages,
+            })
+            .then((date => { }))
+            .catch(err => {
+                console.log(err)
+            })
     }
 
+
+    function hashString(str) {
+        let hash = 0
+        for (let i = 0; i < str.length; i++) {
+            hash += Math.pow(str.charCodeAt(i) * 31, str.length - i)
+            hash = hash & hash //convert to 32bit integer
+        }
+        return hash
+    }
 
 
 
@@ -80,7 +188,7 @@ export default function ChatBox(props) {
                         className='icOpenGallery'
                         src={Images.inputFile}
                         alt='input-file'
-                    // onClick={() => { refInput.click() }}
+                        onClick={() => { refInput.current.click() }}
                     />
                     <img
                         className='viewInputGallery'
@@ -102,10 +210,33 @@ export default function ChatBox(props) {
                         onClick={() => { onSendMessage(inputValue, 0) }}
                     />
                 </div>
+                {isLoading ? (
+                    <div className='viewLoading'>
+                        <ReactLoading
+                            type={'spin'}
+                            color={'#203152'}
+                            height={'3%'}
+                            width={'3%'}
+                        />
+                    </div>
+                ) : null}
             </Card>
+            <input
+                ref={refInput}
+                accept='image/*'
+                className='viewInputFile'
+                type='file'
+                // onChange={onChangeAvatar}
+            />
+
         </>
     )
 }
 
 
 
+/**
+ * questions:
+ * 1. when should i use states vs local variables using useRef?
+ *
+ */
